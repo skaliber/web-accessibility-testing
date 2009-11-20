ce11cbdd29d6ecadc2f1494134a7e394888a30b5
julianharty <julianharty@cbfd35b6-c88d-11de-a006-439b9ae3df91>
2009-11-20 18:51:15 +0000

    I'm checking in a flawed implementation before refactoring the code so others can follow some of the mistakes and corections as the project matures.

--- trunk/javatests/org/julianharty/accessibility/automation/LayoutAndOrdering.java
+++ trunk/javatests/org/julianharty/accessibility/automation/LayoutAndOrdering.java
@@ -27,6 +27,7 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 public class LayoutAndOrdering extends TestCase {
 
 	private static final int MAX_TABS = 100;
+	private static final Point tolerance = new Point(2,2);
 
 	public void testDetectOutOfOrderTabNavigation() throws InterruptedException {
 		FirefoxDriver driver = new FirefoxDriver();
@@ -40,15 +41,88 @@ public class LayoutAndOrdering extends TestCase {
 		tabLayoutTest(driver);
 	}
 
+	/**
+	 * Tests how the reported location of an element varies as the font size grows.
+	 * 
+	 * In theory, I would have expected the offset to either remain constant,
+	 * or to increase as the rendered font size increases. However, the 
+	 * debug text from this test indicates the co-ordinates vary slightly, and
+	 * may increase or decrease after the font size is changed.
+	 */
+	public void testOffsetIncreasesAfterControlPlusKeyStroke() {
+		FirefoxDriver driver = new FirefoxDriver();
+		driver.get("http://localhost/accessibility/explicittabindex.html");
+		RenderedWebElement firstElement = (RenderedWebElement) driver.switchTo().activeElement();
+		RenderedWebElement currentElement = (RenderedWebElement) firstElement;
+		
+		// Go to first tabbable element on page
+		currentElement.sendKeys(Keys.TAB); 
+		currentElement = (RenderedWebElement) driver.switchTo().activeElement();
+		Point initialElementLocation = currentElement.getLocation();
+		System.out.println("testOffsetIncreasesAfterControlPlusKeyStroke");
+		int maxChanges = 5;
+		
+		// First increase the rendered font size
+		Keys modifierKey = Keys.ADD;
+		applyModifierKeyTo(currentElement, maxChanges, modifierKey);
+
+		// Now decrease the rendered font size
+		modifierKey = Keys.SUBTRACT;
+		applyModifierKeyTo(currentElement, maxChanges, modifierKey);
+
+	}
+
+	/**
+	 * A hack that applies a modifier key several times to a web browser.
+	 * 
+	 * The aim is to increase or decrease the rendered font size and determine
+	 * how the reported location of a given web element varies.
+	 * 
+	 * TODO(jharty): enable an ordered set of keys to be passed in by the
+	 * caller e.g. Keys.CONTROL, Keys.ADD
+	 * 
+	 * TODO(jharty): find a way to compare the locations for a set of points. 
+	 * This may become more generalised to allow a caller to pass in either a
+	 * generated set of points (e.g. a result of applying CTRL+ several times)
+	 * or a pre-determined set of points e.g. for known locations of a set of
+	 * elements on a web page.
+	 * 
+	 * TODO(jharty): hmmm, needs refactoring anyway as the comparison of points
+	 * needs to reflect the direction of the change. Consider creating a 
+	 * Direction enum with 2 values: INCREASE, DECREASE, which would obviate
+	 * the need to pass in a set of keystrokes and make the method tightly
+	 * focused.
+	 * 
+	 * @param currentElement the element that currently has focus.
+	 * @param maxChanges the number of times the keystroke will be applied.
+	 * @param modifierKey the key to apply, currently expected to be Keys.ADD
+	 * or Keys.SUBTRACT
+	 */
+	private void applyModifierKeyTo(RenderedWebElement currentElement,
+			int maxChanges, Keys modifierKey) {
+		Point currentElementLocation = currentElement.getLocation();
+		for (int i = 0; i < maxChanges; i++) {
+			currentElement.sendKeys(Keys.CONTROL, modifierKey);
+			Point newLocation = currentElement.getLocation();
+			System.out.println(GeneralHelpers.printElementLocations(i, currentElementLocation, newLocation));
+			
+			if (GeneralHelpers.compareElementLocationsForSaneTabOrder(
+					currentElementLocation, newLocation, tolerance)) {
+				fail(GeneralHelpers.printElementLocations(i, currentElementLocation, newLocation));
+			}
+			currentElementLocation = newLocation;
+		}
+	}
+	
 	private void tabLayoutTest(FirefoxDriver driver)
 			throws InterruptedException {
-		Point tolerance = new Point(2,2);
 		
 		RenderedWebElement firstElement = (RenderedWebElement) driver.switchTo().activeElement();
 		RenderedWebElement currentElement = (RenderedWebElement) firstElement;
 		Point preTabLocation = currentElement.getLocation();
 		int tabsIssued = 0;
 		while (tabsIssued < MAX_TABS) {
+			// currentElement.sendKeys(Keys.CONTROL, Keys.ADD);
 			currentElement.sendKeys(Keys.TAB);  // "\t" also works
 			tabsIssued++;
 			Thread.sleep(50L);
